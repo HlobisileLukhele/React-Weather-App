7a6e2932fec7df9b8aa1d7d64426411d5a16a034
HlobisileLukhele <n.thwala03@gmail.com>
2023-07-03 09:15:13 -0700

    imported Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Weather from './Weather.js'
+import Form from'./Form.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <Form />
       <Weather city="Pretoria"/>
       
       </header>
