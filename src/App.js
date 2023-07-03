81b6c4d8bbf4373cc5efb480e59c67eb2f804f4f
HlobisileLukhele <n.thwala03@gmail.com>
2023-07-03 08:58:41 -0700

    updated city

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Weather/>
+
+      <Weather city="Pretoria"/>
+      
       </header>
     </div>
   );
