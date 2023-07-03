8dcf758ba30285865368422ad31056279672d212
HlobisileLukhele <n.thwala03@gmail.com>
2023-07-03 08:02:24 -0700

    updated alert

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 
-export default function Weather () {
+export default function Weather (props) {
 
     function handleResponse (response) {
-        alert(`The weather in ${}`)
+        alert(`The weather in ${response.data.main.temp}`)
     }
 
     let apiKey= "34ae1065362d42545661451bda2b8a1f"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse)
     return (
