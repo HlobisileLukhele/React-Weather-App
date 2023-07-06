5d8be3aabcbbe67945d17e9f2223f07128f20c51
HlobisileLukhele <n.thwala03@gmail.com>
2023-07-06 00:47:15 -0700

    removed the form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Weather from './Weather.js'
-import Form from'./Form.js'
+
 
 function App() {
   return (
     <div className="App">
       <div className="Container">
       <header className="App-header">
-      <Form />
+      
       <Weather city="Pretoria"/>
       
       </header>
