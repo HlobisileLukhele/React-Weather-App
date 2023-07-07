d186a94139f56d73c26b5255d22596fe0cc37a1b
HlobisileLukhele <n.thwala03@gmail.com>
2023-07-07 16:56:45 -0700

    created if else statement and returned the UI

--- src/Weather.js
+++ src/Weather.js
@@ -29,42 +29,39 @@ export default function Weather (props) {
     function handleCityChange(event) {
         setcity(event.target.value);
     }
-    
 
-return (
-        <div  className="Weather">
-           
-            <form>
-                <div classname="col-9">
-                <input type="search" placeholder="search for a city" className="form-control"
-                />
+    if ( weatherData.ready) {
+        return (
+            <div className="Weather">
+              <form onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-9">
+                    <input
+                      type="search"
+                      placeholder="Enter a city.."
+                      className="form-control"
+                      autoFocus="on"
+                      onChange={handleCityChange}
+                    />
+                  </div>
+                  <div className="col-3">
+                    <input
+                      type="submit"
+                      value="Search"
+                      className="btn btn-primary w-100"
+                    />
+                  </div>
                 </div>
-               <div classname="col-5">
-               <input type="submit" value="search" className="btn btn-primary" w-100 />
-               </div> 
-            </form>
-
-       <h3> New York </h3>
-    <ul>
-        <li> Monday 12:00 </li>
-        <li> Mostly Sunny </li>
-    </ul>
-
-    <div className="row">
-        <div className="col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly cloudy"/>
-            6 C
+              </form>
+              <WeatherInfo data={weatherData} />
+              <WeatherForecast coordinates={weatherData.coordinates} />
             </div>
-            <div className="col-6">
-                <li>Participation: 17%</li>
-                <li>Humidity: 75%</li>
-                <li>Wind: 13kmh</li>
-             </div>   
-        </div>
-  <p> Developed by: Hlobisile lukhele 
-  <a  href="https://github.com/HlobisileLukhele/Vanilla-Weather-App">Source Code</a></p>
-  </div>
-    )
-}
+          );
+        } else {
+          search();
+          return "Loading...";
+        }
+    }
+
+
 
