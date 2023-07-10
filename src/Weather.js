69226ba7e040dfdad35b8ced771e9b756063509d
HlobisileLukhele <n.thwala03@gmail.com>
2023-07-10 00:15:47 -0700

    fixed spelling errors

--- src/Weather.js
+++ src/Weather.js
@@ -4,65 +4,68 @@ import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather (props) {
-    const [weatherData, setWeatherData] = useState({ready:false});
-    const [city, setcity] = useState(props.defaultCity);
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-    function handleResponse(response)  {
-        setWeatherData ({
-            ready : true,
-            coordinates: response.data.coord,
-            temperature: response.data.main.temp,
-            humidity: response.data.main.humidity,
-            date:new Date(response.data.dt* 1000),
-            description: response.data.weather[0].description,
-            icon: response.data.weather[0].icon,
-            wind:response.data.wind.sped,
-            city:response.data.name,
-        });
-    }
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        search();
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-    function handleCityChange(event) {
-        setcity(event.target.value);
-    }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-    if ( weatherData.ready) {
-        return (
-            <div className="Weather">
-              <form onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-9">
-                    <input
-                      type="search"
-                      placeholder="Enter a city.."
-                      className="form-control"
-                      autoFocus="on"
-                      onChange={handleCityChange}
-                    />
-                  </div>
-                  <div className="col-3">
-                    <input
-                      type="submit"
-                      value="Search"
-                      className="btn btn-primary w-100"
-                    />
-                  </div>
-                </div>
-              </form>
-              <WeatherInfo data={weatherData} />
-              <WeatherForecast coordinates={weatherData.coordinates} />
+  function search() {
+    const apiKey = "34ae1065362d42545661451bda2b8a1f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
             </div>
-          );
-        } else {
-          search();
-          return "Loading...";
-        }
-    }
-
-
-
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
